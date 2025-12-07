29990e828dace07e57c12761710fac7319997ede
Igor Jurewicz <igor.jurewicz.wej@gmail.com>
2025-12-07 17:10:26 +0100

    Working on DetrominoData

--- Tetris/Assets/Scripts/Piece.cs
+++ Tetris/Assets/Scripts/Piece.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Board board {get; private set;}
+    public Vector3Int position {get; private set; }
+    public TetrominoData data {get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
+        this.board = board;
+        this.position = position;
+        this.data = data; 
         
     }
 }
